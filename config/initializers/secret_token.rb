37de0c56b2a8a21383d553cf890eeca4f87456f7
Xingnan Zhou <superzxn0705@Xingnans-iMac.local>
2014-01-11 12:27:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlbatrossBackEnd::Application.config.secret_token = 'e4f4a64eeffed0fdfc455373381e98fc60f0769194d1a52cdbc434740369515d92974088f28fa90b3c661a9b2f4a51346b1d60e55c74035083d27994aebf4de6'
